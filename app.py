28d36898dca993496d38dc67d2d71cff45bca46b
Fabricio Silveira <fabvass@br.ibm.com>
2025-01-30 09:52:57 -0300

    upt

--- app.py
+++ app.py
@@ -4,105 +4,23 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
+from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
+from models import db
+from models.models import *
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'
+app.config['SECRET_KEY'] = 'fabricio123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///academia.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 csrf = CSRFProtect(app)
-
-# Modelos
-class Usuario(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    tipo = db.Column(db.String(20), nullable=False)  # 'admin', 'usuario'
-    nome_completo = db.Column(db.String(100))
-    email = db.Column(db.String(120))
-    ativo = db.Column(db.Boolean, default=True)
-    permissoes = db.relationship('Permissao', secondary='usuario_permissao', lazy='subquery',
-        backref=db.backref('usuarios', lazy=True))
-
-    def tem_permissao(self, codigo_permissao):
-        if self.tipo == 'admin':  # Admin tem todas as permissões
-            return True
-        return any(p.codigo == codigo_permissao for p in self.permissoes)
-
-class Permissao(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(50), nullable=False)
-    codigo = db.Column(db.String(50), unique=True, nullable=False)
-
-class Professor(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    cpf = db.Column(db.String(11), unique=True, nullable=False)
-    especialidade = db.Column(db.String(50), nullable=False)
-    telefone = db.Column(db.String(20))
-    email = db.Column(db.String(120))
-
-class ContaPagar(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    descricao = db.Column(db.String(200), nullable=False)
-    valor = db.Column(db.Float, nullable=False)
-    data_vencimento = db.Column(db.Date, nullable=False)
-    status = db.Column(db.String(20), default='pendente')  # 'pendente', 'pago'
-
-class Mensalidade(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    aluno_id = db.Column(db.Integer, db.ForeignKey('aluno.id'), nullable=False)
-    valor = db.Column(db.Float, nullable=False)
-    mes_referencia = db.Column(db.Date, nullable=False)
-    status = db.Column(db.String(20), default='pendente')  # 'pendente', 'pago'
-    data_pagamento = db.Column(db.Date)
-    aluno = db.relationship('Aluno', backref=db.backref('mensalidades', lazy=True))
-
-class Modalidade(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(50), nullable=False)
-    descricao = db.Column(db.Text)
-    valor_mensal = db.Column(db.Float, nullable=False)
-    
-#  Modelo de Plano
-class Plano(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    descricao = db.Column(db.Text)
-    valor_mensal = db.Column(db.Float, nullable=False)
-    ativo = db.Column(db.Boolean, default=True)  # Indica se o plano está ativo
-
-# Tabela de associação entre Aluno e Plano
-aluno_plano = db.Table('aluno_plano',
-    db.Column('aluno_id', db.Integer, db.ForeignKey('aluno.id'), primary_key=True),
-    db.Column('plano_id', db.Integer, db.ForeignKey('plano.id'), primary_key=True),
-    db.Column('data_matricula', db.DateTime, default=datetime.utcnow),
-    db.Column('status', db.String(20), default='ativo')  # Status da matrícula (ativo/trancado)
-)
-
-class Aluno(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    cpf = db.Column(db.String(11), unique=True, nullable=False)
-    data_nascimento = db.Column(db.Date, nullable=False)
-    telefone = db.Column(db.String(20))
-    email = db.Column(db.String(120))
-    modalidades = db.relationship('Modalidade', secondary='aluno_modalidade')
-    planos = db.relationship('Plano', secondary=aluno_plano, lazy='subquery',
-                             backref=db.backref('alunos', lazy=True))
-
-# Tabelas de Associação
-aluno_modalidade = db.Table('aluno_modalidade',
-    db.Column('aluno_id', db.Integer, db.ForeignKey('aluno.id'), primary_key=True),
-    db.Column('modalidade_id', db.Integer, db.ForeignKey('modalidade.id'), primary_key=True)
-)
-
-usuario_permissao = db.Table('usuario_permissao',
-    db.Column('usuario_id', db.Integer, db.ForeignKey('usuario.id'), primary_key=True),
-    db.Column('permissao_id', db.Integer, db.ForeignKey('permissao.id'), primary_key=True)
-)
 
 # Decorator para verificar permissões
 def requer_permissao(codigo_permissao):
@@ -349,8 +267,6 @@ def nova_modalidade():
     if request.method == 'POST':
         modalidade = Modalidade(
             nome=request.form['nome'],
-            descricao=request.form['descricao'],
-            valor_mensal=float(request.form['valor_mensal'])
         )
         db.session.add(modalidade)
         db.session.commit()
@@ -629,6 +545,7 @@ def alterar_status_matricula(aluno_id, plano_id):
 
     return redirect(url_for('listar_matriculas'))
 
+
 # Funções de Inicialização
 def criar_permissoes():
     permissoes = [
@@ -644,8 +561,8 @@ def criar_permissoes():
         if not permissao:
             permissao = Permissao(nome=p['nome'], codigo=p['codigo'])
             db.session.add(permissao)
-    
     db.session.commit()
+
 
 def criar_usuario_admin():
     with app.app_context():
---
+++ models/models.py
@@ -0,0 +1,112 @@
+
+from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from models import db
+from datetime import datetime
+
+
+# Modelos
+class Usuario(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    tipo = db.Column(db.String(20), nullable=False)  # 'admin', 'usuario'
+    nome_completo = db.Column(db.String(100))
+    email = db.Column(db.String(120))
+    ativo = db.Column(db.Boolean, default=True)
+    permissoes = db.relationship('Permissao', secondary='usuario_permissao', lazy='subquery',
+        backref=db.backref('usuarios', lazy=True))
+
+    def tem_permissao(self, codigo_permissao):
+        if self.tipo == 'admin':  # Admin tem todas as permissões
+            return True
+        return any(p.codigo == codigo_permissao for p in self.permissoes)
+
+class Permissao(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    codigo = db.Column(db.String(50), unique=True, nullable=False)
+
+class Professor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    cpf = db.Column(db.String(11), unique=True, nullable=False)
+    especialidade = db.Column(db.String(50), nullable=False)
+    telefone = db.Column(db.String(20))
+    email = db.Column(db.String(120))
+    
+
+class ContaPagar(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    descricao = db.Column(db.String(200), nullable=False)
+    valor = db.Column(db.Float, nullable=False)
+    data_vencimento = db.Column(db.Date, nullable=False)
+    status = db.Column(db.String(20), default='pendente')  # 'pendente', 'pago'
+
+
+class Mensalidade(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    aluno_id = db.Column(db.Integer, db.ForeignKey('aluno.id'), nullable=False)
+    valor = db.Column(db.Float, nullable=False)
+    mes_referencia = db.Column(db.Date, nullable=False)
+    status = db.Column(db.String(20), default='pendente')  # 'pendente', 'pago'
+    data_pagamento = db.Column(db.Date)
+    aluno = db.relationship('Aluno', backref=db.backref('mensalidades', lazy=True))
+
+class Modalidade(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    
+
+#  Modelo de Plano
+class Plano(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.Text)
+    valor_mensal = db.Column(db.Float, nullable=False)
+    ativo = db.Column(db.Boolean, default=True)  # Indica se o plano está ativo
+
+# Tabela de associação entre Aluno e Plano
+aluno_plano = db.Table('aluno_plano',
+    db.Column('aluno_id', db.Integer, db.ForeignKey('aluno.id'), primary_key=True),
+    db.Column('plano_id', db.Integer, db.ForeignKey('plano.id'), primary_key=True),
+    db.Column('data_matricula', db.DateTime, default=datetime.utcnow),
+    db.Column('status', db.String(20), default='ativo')  # Status da matrícula (ativo/trancado)
+)
+
+class Aluno(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    cpf = db.Column(db.String(11), unique=True, nullable=False)
+    data_nascimento = db.Column(db.Date, nullable=False)
+    telefone = db.Column(db.String(20))
+    email = db.Column(db.String(120))
+    matriculas = db.relationship('Matricula', backref='aluno')
+
+
+usuario_permissao = db.Table('usuario_permissao',
+    db.Column('usuario_id', db.Integer, db.ForeignKey('usuario.id'), primary_key=True),
+    db.Column('permissao_id', db.Integer, db.ForeignKey('permissao.id'), primary_key=True)
+)
+
+class Turma(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    
+    # Relação com Modalidade
+    modalidades = db.relationship('Modalidade', secondary='turma_modalidade', backref='turmas')
+    
+
+# Tabela de Associação entre Turmas e Modalidades
+turma_modalidade = db.Table('turma_modalidade',
+    db.Column('turma_id', db.Integer, db.ForeignKey('turma.id'), primary_key=True),
+    db.Column('modalidade_id', db.Integer, db.ForeignKey('modalidade.id'), primary_key=True)
+)
+
+
+class Matricula(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    aluno_id = db.Column(db.Integer, db.ForeignKey('aluno.id'), nullable=False)
+    plano_id = db.Column(db.Integer, db.ForeignKey('plano.id'), nullable=False)
+    status = db.Column(db.String(20), default='ativo')  # 'ativo', 'trancado'
+    data_matricula = db.Column(db.DateTime, default=datetime.utcnow)
